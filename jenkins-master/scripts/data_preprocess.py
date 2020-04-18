a72621014680736919c08d854664bc0727618850
donggul <donggul@sfu.ca>
2020-04-18 10:08:44 -0700

    Fixed bugs

--- jenkins-master/scripts/data_preprocess.py
+++ jenkins-master/scripts/data_preprocess.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import os
 import sys
 import bz2
@@ -13,10 +10,6 @@ import numpy as np
 from nltk import download, tokenize, word_tokenize 
 from nltk.corpus import stopwords
 from gensim.models import Word2Vec
-from gensim.test.utils import get_tmpfile
-
-# nltk.download('stopwords')
-# nltk.download('punkt')
 
 stop_words = stopwords.words('english')
 
@@ -77,6 +70,7 @@ if __name__ == '__main__':
     with bz2.open(filepath) as file:
         doc = xmltodict.parse(file.read())
     df_text = pd.DataFrame(columns=['title', 'text', 'wiki_link', 'redirect'])
+    err_list = []
     
     # Reading in each article
     for page in doc['mediawiki']['page']:
@@ -86,32 +80,35 @@ if __name__ == '__main__':
         wiki_link = ''
         redirect = 'F'
 
-        if 'redirect' in page:
-            # only keeping redirecting link
-            txt = re.search('(\[\[(.*?)\]\])', page['revision']['text']['#text']).group(1)
-            txt = re.sub('\[*\]*', '', txt)
-            redirect = 'T'
-            wiki_link = txt.strip()
+        try:
+            if 'redirect' in page:
+                # only keeping redirecting link
+                txt = re.search('(\[\[(.*?)\]\])', page['revision']['text']['#text']).group(1)
+                txt = re.sub('\[*\]*', '', txt)
+                redirect = 'T'
+                wiki_link = txt.strip()
 
-        else:
-            # getting rid of {{~}}, [[File:~]], <!-- ~ -->, <ref ~ />, <ref ~</ref>, <br~>
-            txt = re.sub(r'({{(.*?)}})|(\[\[File:(.*?)\n)|(\<\!\-\-(.*?)\-\-\>)|(\<ref(.*?)\/\>)|(\<ref(.*?)\<\/ref\>)|(\<br(\s?\/?)\>)', 
-                         '', page['revision']['text']['#text'], 0, re.DOTALL)
+            else:
+                # getting rid of {{~}}, [[File:~]], <!-- ~ -->, <ref ~ />, <ref ~</ref>, <br~>
+                txt = re.sub(r'({{(.*?)}})|(\[\[File:(.*?)\n)|(\<\!\-\-(.*?)\-\-\>)|(\<ref(.*?)\/\>)|(\<ref(.*?)\<\/ref\>)|(\<br(\s?\/?)\>)', '', page['revision']['text']['#text'], 0, re.DOTALL)
 
-            # separating internal links
-            link = re.findall('(\[\[(.*?)\]\])', txt)
-            text_w = re.sub('(\[\[(.*?)\]\])|(\\n)', ' ', txt, 0, re.DOTALL)
-            text_s = re.sub('(?<=^\[\[\b).*(?=\b\|(.*?)\]\])|(\\n)',' ',txt, 0, re.DOTALL) #keeping the links first
-            text_s = re.sub('(\[\[(.*?)\]\])|(\\n)', ' ', text_s, 0, re.DOTALL)
+                # separating internal links
+                link = re.findall('(\[\[(.*?)\]\])', txt)
+                text_w = re.sub('(\[\[(.*?)\]\])|(\\n)', ' ', txt, 0, re.DOTALL)
+                text_s = re.sub('(?<=^\[\[\b).*(?=\b\|(.*?)\]\])|(\\n)',' ',txt, 0, re.DOTALL) #keeping the links first
+                text_s = re.sub('(\[\[(.*?)\]\])|(\\n)', ' ', text_s, 0, re.DOTALL)
 
-            for c in link:
-                if '|' in c[1]:
-                    sep = c[1].split('|')
-                    wiki_link = wiki_link + ', ' + sep[0]
-                    text_w = text_w + ', ' + sep[1]
-                else:
-                    wiki_link = wiki_link + ', ' + c[1]
-                    text_w = text_w + ', ' + c[1]
+                for c in link:
+                    if '|' in c[1]:
+                        sep = c[1].split('|')
+                        wiki_link = wiki_link + ', ' + sep[0]
+                        text_w = text_w + ', ' + sep[1]
+                    else:
+                        wiki_link = wiki_link + ', ' + c[1]
+                        text_w = text_w + ', ' + c[1]
+        except:
+            err_list.append(page)
+
 
         df_text = df_text.append({'title': title, 'text': text_w, 'wiki_link': wiki_link, 'redirect': redirect, 'sentences': text_s}, ignore_index=True) 
 
@@ -121,8 +118,8 @@ if __name__ == '__main__':
 
     ##Centroid Word Vector Calculation
     print('Beginning Word Vector Calculation')
-    path = get_tmpfile("word2vec.model")
     model = Word2Vec(sentences = df_text['text'], min_count=0, window=2)
+    model.save('word_emb.model')
     df_text['centroid_vec'] =  df_text['text'].apply(get_centroid_vec)
     
     # Write CSV
